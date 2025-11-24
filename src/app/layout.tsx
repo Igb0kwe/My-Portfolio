021758dd446c9ca9397d225d12d4288dd2ac451b
Kagozie Ikechukwu Igbokwe <kagozieigbokwe@gmail.com>
2025-11-24 11:29:47 +0100

    speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Kagozie Igbokwe',
@@ -25,8 +26,9 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
-        <Toaster />        
+        <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
